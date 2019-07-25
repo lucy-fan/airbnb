067b3c68a46e0041204d806dfe0a28f1d8835206
Lucy Fan <39575038+lucy-fan@users.noreply.github.com>
2019-07-25 23:52:23 +0100

    Update booking.service.ts

--- consumer-mobile-app/src/app/services/booking.service.ts
+++ consumer-mobile-app/src/app/services/booking.service.ts
@@ -1,10 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Booking } from '../models/booking';
-import { Listing } from '../models/listing';
-import { ListingService } from '../services/listing.service'
-import { User } from '../models/user';
-import { UserService } from '../services/user.service';
 
 @Injectable({
   providedIn: 'root'
@@ -13,20 +9,13 @@ import { UserService } from '../services/user.service';
 export class BookingService {
   bookings: Array<Booking> = [];
   booking: Booking = new Booking();
-  listing: Listing = new Listing();
-  user: User = new User();
 
   constructor(
     private http: HttpClient,
     private userService: UserService,
     private listingService: ListingService
   ) 
-  { 
-    this.user = this.userService.getUser();
-    console.log("user id is: " + this.user.id);
-    this.listing = this.listingService.getListing();
-    console.log("listing id is: " + this.listing.id);
-  }
+  { }
 
   setBooking(booking: Booking) {
     this.booking = booking;
